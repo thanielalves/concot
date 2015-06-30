64f1adcf90246d94257884ad17dbe7c598cc92df
thanielalves <thanielalves@gmail.com>
2015-06-30 18:01:18 -0300

    Apenas corrigindo uma inversão de códigos.

--- grails-app/domain/concot/Item.groovy
+++ grails-app/domain/concot/Item.groovy
@@ -4,12 +4,13 @@ class Item {
 
     String nome
     
+    // Modificando o modo como a representação textual é gerada
     String toString() {
         this.nome
     }
     
     static belongsTo = [categoria:Categoria]
-    
+
     static constraints = {
         nome nullable:false, blank:false, maxSize:128
         categoria nullable:false
