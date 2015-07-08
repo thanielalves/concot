2c2dcaf2ba28b86f8ada53acc6fbadf79cbc3efe
thanielalves <thanielalves@gmail.com>
2015-07-08 18:11:26 -0300

    Testes Unitários e de Integração.

---
+++ grails-app/domain/concot/LimiteCotacao.groovy
@@ -0,0 +1,14 @@
+package concot
+
+class LimiteCotacao {
+	
+	BigDecimal valorMinimo
+	BigDecimal valorMaximo
+
+	static belongsTo = [lista:ListaCotacoes, item:Item, moeda:Moeda]
+
+	static constraints = {
+		valorMinimo: nullable:true
+		valorMaximo: nullable:true
+	}
+}
